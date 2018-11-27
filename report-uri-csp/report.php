c1fdc6551dfca6d07634929d1bc38460924f8ee6
O.Leier <git.OLr@ib-leier.net>
2018-11-27 02:55:02 +0100

    with log file

--- report-uri-csp/report.php
+++ report-uri-csp/report.php
@@ -5,10 +5,16 @@
 
 // https://websec.be/blog/cspreporting/
 // https://content-security-policy.com/
-// https://www.tollmanz.com/content-security-policy-report-samples/
+// https://www.tollmanz.com/content-security-policy-report-samples/ -> https://github.com/tollmanz/report-only-capture
 
 // report-uri endpoint example
 // https://shaunc.com/blog/article/implementing-a-reporturi-endpoint-for-expectct-and-other-headers~Xdf4cU8EurV1
+
+// https://www.phpgangsta.de/die-reporting-funktion-der-content-security-policy-csp
+// https://www.w3schools.com/php/php_superglobals.asp
+
+  ini_set("display_errors",TRUE);
+  error_reporting(E_ALL);
 
 // Start configure
 $log_file = dirname(__FILE__) . '/csp-violations.log';
@@ -19,8 +25,26 @@ $email_subject = 'Content-Security-Policy violation';
 
 $current_domain = preg_replace('/www\./i', '', $_SERVER['SERVER_NAME']);
 $email_subject = $email_subject . ' on ' . $current_domain;
+$remote_name = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 
 http_response_code(204); // HTTP 204 No Content
+
+//echo "log_file: $log_file</br>";
+//echo "current_domain: $current_domain</br>";
+//$rc = file_put_contents($log_file, "json_data\n", FILE_APPEND | LOCK_EX);
+//echo "file_put_contents rc: $rc</br>";
+
+//Grab the headers, too
+$headers = var_export(getallheaders(), true);
+echo "headers: $headers</br>";
+//$request = var_export($_REQUEST, true);
+//echo "request: $request</br>";
+$post = var_export($_POST, true);
+echo "post: $post</br>";
+$get = var_export($_GET, true);
+echo "get: $get</br>";
+$server = var_export($_SERVER, true);
+//echo "server: $server</br>";
 
 $json_data = file_get_contents('php://input');
 
@@ -31,17 +55,39 @@ if ($json_data = json_decode($json_data)) {
   if (!file_exists($log_file)) {
     // Send an email
     $message = "The following Content-Security-Policy violation occurred on " .
-      $current_domain . ":\n\n" .
-      $json_data .
+      $current_domain . " by " . $_SERVER['REMOTE_ADDR'] . "(" . $remote_name . "):\n\n" .
+      $json_data . "\n\n" .
+      "headers: " . $headers . "\n\n" .
+      // "request: " . $request . "\n\n" .
+      "post: " . $post . "\n\n" .
+      "get: " . $get . "\n\n" .
+      "server: " . $server . "\n\n" .
       "\n\nFurther CPS violations will be logged to the following log file, but no further email notifications will be sent until this log file is deleted:\n\n" .
       $log_file;
     mail($email_address, $email_subject, $message,
          'Content-Type: text/plain;charset=utf-8');
+		 
   } else if (filesize($log_file) > $log_file_size_limit) {
     exit(0);
   }
 
-  file_put_contents($log_file, $json_data, FILE_APPEND | LOCK_EX);
 }
+
+//date_default_timezone_set('Europe/Berlin');
+echo 'date_default_timezone_set: ' . date_default_timezone_set('Europe/Berlin') . '<br />';
+//echo 'date_default_timezone_get: ' . date_default_timezone_get() . '<br />';
+
+// Current date/time in your computer's time zone.
+//$date = new DateTime();
+//echo $date->format('Y-m-d H:i:sP') . "<br />\n";	// 2018-07-07 21:41:44+02:00 
+
+$date = new DateTime("now");
+//echo $date->format('Y-m-d\TH:i:s.u') . '<br />';	// 2011-01-01T15:03:01.012345 / 2018-07-07T21:46:15.000000
+//echo $date->format(DateTime::ATOM) . '<br />';		// 2018-07-07T21:46:15+02:00
+
+$info =	"\n\n" . $date->format(DateTime::ATOM) . " - " . $_SERVER['REQUEST_METHOD'] .
+		"\nOn " . $current_domain . " by " . $_SERVER['REMOTE_ADDR'] . "(" . $remote_name . ")\n" .
+		$_SERVER['HTTP_USER_AGENT'] . "\n";
+file_put_contents($log_file, $info . $json_data, FILE_APPEND | LOCK_EX);
 
 ?>
